bf027807ab3e8bba08b382212ecceadde0cf9810
Newton Lomar <newtonlomar16@gmail.com>
2021-10-12 13:31:53 -0300

    updating console visualization

--- script.js
+++ script.js
@@ -23,19 +23,19 @@ const currentYear = new Date().getFullYear()
 const isAtLeastOneNineteenOrOlder = people.some(people => currentYear - people.year >= 19)
 console.log(`is at least one person 19 or older?`, isAtLeastOneNineteenOrOlder)
 
-
+console.log('')
 // Array.prototype.every() // is everyone 19 or older?
 const isEveryOneNineteenOrOlder = people.every(person => currentYear - person.year >= 19)
 console.log(`is everyone 19 or older?`, isEveryOneNineteenOrOlder)
 
-
+console.log('')
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 const find = comments.find(comment => comment.id === 823423)
 console.log('find the comment with the ID of 823423', find)
 
-
+console.log('')
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
@@ -46,6 +46,7 @@ console.log('Find index of the comment with this ID',  findIndex)
 // comments.splice(findIndex, 1)
 // console.log(comments)
 
+console.log('delete the comment with the ID of 823423 - ')
 const newComments = [
   ...comments.slice(0, 1),
   ...comments.slice(2, comments.length)
